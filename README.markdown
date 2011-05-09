feb4a6cadbca53378ca8e5c8ab5edd02c13c6185
Paul Sexton <psexton@gnresound.com>
2011-05-09 12:40:43 -0500

    Initial commit.

---
+++ README.markdown
@@ -0,0 +1,4 @@
+LifeSaverDedupe Readme
+======================
+
+This is a tool for deleting duplicate entries in a backup created by Tim Bray's LifeSaver2 tool.
---
+++ src/net/psexton/lifesaverdedupe/LifeSaverDedupe.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.psexton.lifesaverdedupe;
+
+/**
+ *
+ * @author PSexton
+ */
+public class LifeSaverDedupe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
